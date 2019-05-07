3e05f61f3551cd1417c779307fa19c1319b9f860
DStadnikov <c.d.29_94@mail.ru>
2019-05-07 11:40:25 +0800

    Commit

--- webapp/controller/V_Header.controller.js
+++ webapp/controller/V_Header.controller.js
@@ -8,23 +8,22 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller) {
 		 */
 		onInit: function() {
 			var oView = this.getView();
-		
 			var input = oView.byId("inputSel");
 			input.bindAggregation("suggestionItems", {
 				path: "/stbegSet",
 				template: new sap.ui.core.Item({
-					text: "{st_spr}"
+					key: "{st_spr}",
+					text: "{Txtlg} - {st_spr}"
 				})
 			});
-			
 			var inputEnd = oView.byId("inputEndStation");
 			inputEnd.bindAggregation("suggestionItems", {
 				path: "/stbegSet",
 				template: new sap.ui.core.Item({
-					text: "{st_spr}"
+					key: "{st_spr}",
+					text: "{Txtlg} - {st_spr}"
 				})
 			});
-
 			var inputCode = oView.byId("codeCargo");
 			inputCode.bindAggregation("suggestionItems", {
 				path: "/codeCargoSet",
@@ -32,33 +31,14 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller) {
 					text: "{kodgrgr}"
 				})
 			});
+			input.setFilterFunction(function(sTerm, oItem) {
+				// A case-insensitive 'string contains' style filter
+				return oItem.getText().match(new RegExp(sTerm, "i"));
+			});
 
 			var myModel = this.getOwnerComponent().getModel();
 			myModel.setSizeLimit(10000);
 		},
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf APP12FirstLook.view.V_Header
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf APP12FirstLook.view.V_Header
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf APP12FirstLook.view.V_Header
-		 */
-		//	onExit: function() {
-		//
-		//	}
 		/**
 		 *@memberOf APP12FirstLook.controller.V_Header
 		 */
@@ -87,13 +67,32 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller) {
 		 *@memberOf APP12FirstLook.controller.V_Header
 		 */
 		suggestSet: function(oEvent) {
-
 			/*	input.addSuggestionItem(
-				new sap.ui.core.Item({
-					key: '1',
-					text: '123'
-				})
-			);*/
+										new sap.ui.core.Item({
+											key: '1',
+											text: '123'
+										})
+									);*/
+		},
+		/**
+		 *@memberOf APP12FirstLook.controller.V_Header
+		 */
+		begSuggest: function(oEvent) {
+			var item = oEvent.getParameter("selectedItem");
+			var key = item.getKey();
+
+			var input = this.getView().byId("inputSel");
+			input.setValue(key);
+		},
+		/**
+		 *@memberOf APP12FirstLook.controller.V_Header
+		 */
+		endSuggest: function(oEvent) {
+			var item = oEvent.getParameter("selectedItem");
+			var key = item.getKey();
+
+			var input = this.getView().byId("inputEndStation");
+			input.setValue(key);
 		}
 	});
 });
